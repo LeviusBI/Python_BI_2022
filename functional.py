e8ab85b5bf7adc080fb81bcf2a813e75e090fd12
lev <levlewleff@gmail.com>
2022-12-01 19:53:22 +0300

    Added last function and included it into 1st function

--- functional.py
+++ functional.py
@@ -2,80 +2,91 @@
 # sequential_map - функция должна принимать в качестве аргументов любое количество функций 
 # (позиционными аргументами, НЕ списком), а также контейнер с какими-то значениями. 
 # Функция должна возвращать список результатов последовательного применения переданных функций 
-# к значениям в контейнере. Например, 
-# sequential_map(np.square, np.sqrt, lambda x: x**3, [1, 2, 3, 4, 5]) -> [1, 8, 27, 64, 125]
+# к значениям в контейнере. 
 def sequential_map(*args):
     *functions_list, coll = [*args]
     for i in functions_list:
         coll = list(map(i, coll))
     return coll
-    
-    
+
+print(sequential_map(lambda x: x**2, lambda x: x**(0.5), lambda x: x**3, [1, 2, 3, 4, 5]))
 
 # consensus_filter - функция должна принимать в качестве аргументов любое количество функций 
 # (позиционными  аргументами, НЕ списком), возвращающих True или False, а также контейнер с какими-то
 #  значениями. Функция должна возвращать список значений, которые при передаче их во все функции 
-# дают True. Например: consensus_filter(lambda x: x > 0, lambda x: x > 5, lambda x: x < 10, 
-# [-2, 0, 4, 6, 11]) -> [6]
+# дают True.
 def consensus_filter(*args):
     *functions_list, coll = [*args]
     for i in functions_list:
         coll = list(filter(i, coll))
     return coll
 
+print(consensus_filter(lambda x: x > 0, lambda x: x > 5, lambda x: x < 10, [-2, 0, 4, 6, 11]))
 
 # conditional_reduce - функция должна принимать 2 функции, а также контейнер с значениями. 
 # Первая функция должна принимать 1 аргумент и возвращать True или False, вторая также принимает 2 
 # аргумента и возвращает значение (как в обычной функции reduce). conditional_reduce должна возвращать
 #  одно значение - результат reduce, пропуская значения с которыми первая функция выдала False. 
-# Например, conditional_reduce(lambda x: x < 5, lambda x, y: x + y, [1, 3, 5, 10]) -> 4
 def conditional_reduce(*args):
     *functions_list, coll = [*args]
     coll = list(filter(functions_list[0], coll))
-    while len(coll) != 1:
-        coll[0]=functions_list[1](coll[0], coll[1])
-        del coll[1]
-    return coll
-       
-res = conditional_reduce(lambda x: x < 5, lambda x, y: x + y, [1, 3, 5, 10])
-print(res)        
-    
-
+    if len(coll) > 1:
+        while len(coll) != 1:
+            coll[0]=functions_list[1](coll[0], coll[1])
+            del coll[1]
+        return coll
+    else:
+        return coll
+print(conditional_reduce(lambda x: x < 5, lambda x, y: x + y, [1, 3, 5, 10]))              
 
 # func_chain - функция должна принимать в качестве аргументов любое количество функций (позиционными 
 #  аргументами, НЕ списком). Функция должна возвращать функцию, объединяющую все переданные 
-# последовательным выполнением. Например, 
-# my_chain = func_chain(lambda x: x + 2, lambda x: (x/4, x//4)). my_chain(37) -> (9.75, 9). 
-# +2 дополнительных балла за интеграцию этой функции в 1 задание.
+# последовательным выполнением.
 def func_chain(*args):
     def run_pipe(x: int):
-        res = x
-        for f in args:
-            res = f(res)
-        return res
+        if type(x) == int or type(x) == float:
+            res = x
+            for f in args:
+                res = f(res)
+            return res
+        elif type(x) == list: #чтоб работала на списках наверняка
+            res = x
+            for i in range(0, len(x)):
+                for f in args:
+                    res[i] = f(res[i])
+            return res
     return run_pipe
-    
-    
 
+# Интеграция func_chain в первую функцию 
 
+def sequential_map_2(*args):
+    *functions_list, coll = [*args]
+    pipeline = func_chain(*functions_list)
+    return pipeline(coll)
+print(sequential_map_2(lambda x: x**2, lambda x: x**(0.5), lambda x: x**3, [1, 2, 3, 4, 5]))
 # Реализовать функцию  multiple_partial - аналог функции partial, но которая принимает неограниченное 
 # число функций в качестве аргументов и возвращает список из такого же числа "частичных функций". 
 # Не используйте саму функцию partial. Например: 
 # ax1_mean, ax1_max, ax1_sum = multiple_partial(np.mean, np.max, np.sum, axis=1)
 def multiple_partial(*args, **kwargs):
-    def func_maker(*args, **kwargs):
-        func_list = [*args]
-        kwd = {**kwargs}
-        ret_func_list = []
-        for func in func_list:
-            return func(**kwargs)
-    return func_maker
+    func_list = [*args]
+    kwd = {**kwargs}
+    ret_func_list = []
+    for func in func_list:
+        ret_func_list
+    return ret_func_list
 # Реализуйте полный аналог функции print без использования её самой (без аргумента flush).
 # В этом деле вам может помочь встроенный модуль sys.
-def personal_print(*args, sep = ' ', end = '\n', file = None):
-    if file == None:
-        return f"{sep}".join([*args])+f"{end}"
-    
+import sys as s 
+def personal_print(*args: str, sep = '', end = '\n', file = s.stdout):
+    if file == s.stdout:
+        result = ""
+        for i in args:
+            result += f"{i}{sep}"
+        return "result %{end}"
+    else: 
+        pass
+ 
 # Print objects to the text stream file, separated by sep and followed by end. sep, end, file, 
 # and flush, if present, must be given as keyword arguments.
 
