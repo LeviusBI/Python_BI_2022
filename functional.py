13ebacb3d624dfaf128d65ea2fbf0d68277ba65f
lev <levlewleff@gmail.com>
2022-12-02 05:10:53 +0300

    added personal print

--- functional.py
+++ functional.py
@@ -41,37 +41,22 @@ def func_chain(*args):
 
 
 def multiple_partial(*args, **kwargs):
-    def func_maker(func, **kwargs):
-        return lambda x: func(x, **kwargs)
+    def func_maker(f, **kwargs):
+        return lambda x: f(x, **kwargs)
     ret_func_list = []
-    for func in args:
-        res = func_maker(func, **kwargs)
+    for f in args:
+        res = func_maker(f, **kwargs)
         ret_func_list.append(res)
     return ret_func_list
-# Реализуйте полный аналог функции print без использования её самой (без аргумента flush).
-# В этом деле вам может помочь встроенный модуль sys.
-import sys as s 
+
+import sys 
 def personal_print(*args: str, sep=' ', end='\n', file = None):
     if file == None:
         result = ""
         for i in args:
             result += f"{i}{sep}"
-        s.stdout.write()
-    else: 
-        pass
-
-# Print objects to the text stream file, separated by sep and followed by end. sep, end, file, 
-# and flush, if present, must be given as keyword arguments.
-
-# All non-keyword arguments are converted to strings like str() does and written to the stream, 
-# separated by sep and followed by end. Both sep and end must be strings; they can also be None, 
-# which means to use the default values. If no objects are given, print() will just write end.
-
-# The file argument must be an object with a write(string) method; if it is not present or None, 
-# sys.stdout will be used. Since printed arguments are converted to text strings, print() 
-# cannot be used with binary mode file objects. For these, use file.write(...) instead.
-
-# Whether the output is buffered is usually determined by file, but if the flush keyword argument 
-# is true, the stream is forcibly flushed.
-
-# Changed in version 3.3: Added the flush keyword argument.    
+        result+=f"{end}"
+        sys.stdout.write(result)
+    else:
+        with open(file, 'w') as sys.stdout:
+            personal_print(*args, sep, end)
