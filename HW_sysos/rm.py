5d0ca453160ea0411927c2a768121998fa1e5890
lev <levlewleff@gmail.com>
2022-12-18 15:42:21 +0300

    tryed to modify scripts

--- HW_sysos/rm.py
+++ HW_sysos/rm.py
@@ -6,38 +6,3 @@ import argparse
 
 # option -r - recursive deletion
 
-def rm_args():
-    parser = argparse.ArgumentParser(
-        prog = 'rm.py [OPTION].. [FILE]...',
-        description = ''' Remove (unlink) the FILE(s).\n
-        
-        By default, rm does not remove directories.  Use the --recursive (-r or -R)\n
-        option to remove each listed directory, too, along with all of its contents.\n
-        To remove a file whose name starts with a '-', for example '-foo',\n
-        use one of these commands:\n
-        rm -- -foo\n
-
-        rm ./-foo\n
-        ''' 
-    )
-    parser.add_argument('-r', '-R', '--recursive')
-    
-    parser.add_argument('files', type=str, default=None, nargs='*',
-                        help='files to delete')
-    return parser.parse_args()
-
-
-if __name__ == '__main__':
-    del_functions = {False: os.remove,
-                     True: os.rmdir}
-
-    args = parse_rm_args()
-
-    for file in args.files:
-        try:
-            is_dir = args.recursive and os.path.isdir(file)
-            del_functions[is_dir](file)
-        except (NotADirectoryError, FileNotFoundError):
-            sys.stderr.write(f'rm: cannot remove \'{file}\': No such file or directory\n')
-        except IsADirectoryError:
-            sys.stderr.write(f'rm: cannot remove \'{file}\': Is a directory\n')
--- HW_sysos/wc.py
+++ HW_sysos/wc.py
@@ -7,7 +7,7 @@ import argparse
 # option -l - show number of lines in file
 # option -c - show quantity of bytes in file
 # option -w - show number of words in file
-def main():
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             usage='./wc.py [OPTION]... [FILE]...',
             description='''Print newline, word, and byte counts for each FILE, and a total line 
@@ -38,50 +38,23 @@ def main():
     args = parser.parse_args()
         
     filename = args.filename
+    
+    file_content = args.filename.read()
         
-    def extract_actions_list(args):
-        del args['filename']
-        if not any(args.values()):
-            return list(args.keys())
-        else:
-            return [action for action, is_needed in args.items() if is_needed]
-
-
     def lines_counter(data):
         return len(open(data).readlines())
 
 
-    def count_words(data):
+    def words_counter(data):
         return len(data.split())
 
 
-    def count_bytes(data):
+    def byte_syze(data):
         return os.path.getsize(data)
         
-    file_content = args.filename.read()
-    actions = extract_actions_list(vars(args))
 
     functions = {'lines': lines_counter,
-                'words': count_words,
-                'bytes': count_bytes
+                'words': words_counter,
+                'bytes': byte_syze
                 }
-
-    results = []
-    for action in actions:
-        results.append(functions[action](file_content))
-
-    sys.stdout.write('\t' + '\t'.join(map(str, results)) + '\n')
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    try:
-       main()
-    except SystemExit:
-       sys.exit(1)
-    else:
-        sys.exit(0)
+    
